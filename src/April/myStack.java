f7fe8c7ab56fb34487eae167d7b70551f2681fce
Gonghan <wanggonghan@gmail.com>
2014-04-14 15:19:18 -0700

    add mystack<T>

--- src/April/myStack.java
+++ src/April/myStack.java
@@ -1,5 +1,37 @@
 package April;
 
-public class myStack {
+public class myStack<T> {
 
+	myLinkedList<stackNode<T>>stack;
+	
+	
+	
+	boolean empty(){
+		return stack.empty();
+	}
+	
+	stackNode<T> peek(){
+		return stack.getLast().value;
+	}
+	
+	stackNode<T> pop(){
+		stackNode<T> node =stack.getLast().value;
+		stack.removeLast();
+		return node;
+	}
+	
+	void push(T value){
+		stack.append(new stackNode<T>(value));
+	}
+	
+	public static void main(String args[]){
+		
+	}
+}
+
+class stackNode<T>{
+	T value;
+	stackNode(T value){
+		this.value=value;
+	}
 }
