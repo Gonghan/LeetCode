c66af286060f206f9f4b2f0499a04db12468c92e
Gonghan <wanggonghan@gmail.com>
2014-04-14 15:26:01 -0700

    add a test case for stack, and format the code

--- src/April/myStack.java
+++ src/April/myStack.java
@@ -2,36 +2,50 @@ package April;
 
 public class myStack<T> {
 
-	myLinkedList<stackNode<T>>stack;
+	myLinkedList<stackNode<T>> stack;
 	
 	
-	
-	boolean empty(){
+
+	public myStack() {
+		this.stack = new myLinkedList<stackNode<T>>();
+	}
+
+	boolean empty() {
 		return stack.empty();
 	}
-	
-	stackNode<T> peek(){
+
+	stackNode<T> peek() {
 		return stack.getLast().value;
 	}
-	
-	stackNode<T> pop(){
-		stackNode<T> node =stack.getLast().value;
+
+	stackNode<T> pop() {
+		stackNode<T> node = stack.getLast().value;
 		stack.removeLast();
 		return node;
 	}
-	
-	void push(T value){
+
+	void push(T value) {
 		stack.append(new stackNode<T>(value));
 	}
-	
-	public static void main(String args[]){
-		
+
+	public static void main(String args[]) {
+		myStack<Integer> stack = new myStack<Integer>();
+		stack.push(1);
+		stack.push(3);
+		stack.push(2);
+		System.out.println(stack.peek().value);
+		System.out.println(stack.pop().value);
+		System.out.println(stack.empty());
+		System.out.println(stack.pop().value);
+		System.out.println(stack.pop().value);
+		System.out.println(stack.empty());
 	}
 }
 
-class stackNode<T>{
+class stackNode<T> {
 	T value;
-	stackNode(T value){
-		this.value=value;
+
+	stackNode(T value) {
+		this.value = value;
 	}
 }
